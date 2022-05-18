d7b36587a610a9194fc50edb8270877275038c30
MakSoo98 <Marcosferrerhernandez@gmail.com>
2022-05-18 22:07:08 +0200

    he vuelto a poner el formulario
    he vuelto a poner el formulario

---
+++ SCVud10v2/SCVud10v2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace SCVud10v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tNombre = new System.Windows.Forms.TextBox();
+            this.tApellido = new System.Windows.Forms.TextBox();
+            this.tLista_Socios = new System.Windows.Forms.TextBox();
+            this.tTelefono = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Telefono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Lista Socios";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(903, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tNombre
+            // 
+            this.tNombre.Location = new System.Drawing.Point(118, 30);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(100, 20);
+            this.tNombre.TabIndex = 5;
+            // 
+            // tApellido
+            // 
+            this.tApellido.Location = new System.Drawing.Point(118, 72);
+            this.tApellido.Name = "tApellido";
+            this.tApellido.Size = new System.Drawing.Size(100, 20);
+            this.tApellido.TabIndex = 6;
+            // 
+            // tLista_Socios
+            // 
+            this.tLista_Socios.Location = new System.Drawing.Point(118, 154);
+            this.tLista_Socios.Multiline = true;
+            this.tLista_Socios.Name = "tLista_Socios";
+            this.tLista_Socios.ReadOnly = true;
+            this.tLista_Socios.Size = new System.Drawing.Size(860, 280);
+            this.tLista_Socios.TabIndex = 8;
+            // 
+            // tTelefono
+            // 
+            this.tTelefono.Location = new System.Drawing.Point(118, 112);
+            this.tTelefono.Name = "tTelefono";
+            this.tTelefono.Size = new System.Drawing.Size(100, 20);
+            this.tTelefono.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1027, 485);
+            this.Controls.Add(this.tLista_Socios);
+            this.Controls.Add(this.tTelefono);
+            this.Controls.Add(this.tApellido);
+            this.Controls.Add(this.tNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tNombre;
+        private System.Windows.Forms.TextBox tApellido;
+        private System.Windows.Forms.TextBox tLista_Socios;
+        private System.Windows.Forms.TextBox tTelefono;
+    }
+}
+
